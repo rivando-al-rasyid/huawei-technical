429f1abe3a6e59271cce57e89221636e5c939dfb
rivando-al-rasyid <nougat-tutorial@gmail.com>
2026-07-17 17:45:57 +0700

    feat : post add user and user list

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
 
+const users = [];
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.post("/user/add", (req, res) => {
+  users.push(req.body);
+
+  res.json({
+    message: "Data berhasil disimpan",
+  });
 });
 
-app.post("/", (req, res) => {
-  res.send(`Got a POST request ${req.body}`);
+app.get("/user/list", (req, res) => {
+  res.json(users);
 });
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
