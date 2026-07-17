b2b4f0bc5107670dcb4b0b0a1acd17045db82ae6
rivando-al-rasyid <nougat-tutorial@gmail.com>
2026-07-17 18:05:02 +0700

    cron jobs

---
+++ cron.sh
@@ -0,0 +1,15 @@
+# declare the cron job schedule use sh 
+#!/bin/bash
+
+# get current date and time for filename
+DATE=$(date +"%m%d%Y")
+TIME=$(date +"%H.%M")
+
+# create a filename with the current date and time on folder files
+FILENAME="${PWD}/files/cron_${DATE}_${TIME}.csv"
+
+#get the resource URL
+RESOURCE_URL="localhost:3000/user/list" 
+
+# Collect data from the resource URL and save it to the file
+curl -s "$RESOURCE_URL" -o "$FILENAME"
