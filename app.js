64c0e6edfa7d73cdea955ad799560dbf9de17d34
Rivando Al rasyid <119521535+rivando-al-rasyid@users.noreply.github.com>
2026-07-17 09:41:40 +0000

    feat : post

--- app.js
+++ app.js
@@ -3,28 +3,17 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-// Parse application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
 
-// Parse JSON (useful for future API requests)
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.post("/", (req, res) => {
-  console.log(req.body);
-
-  const { username, password } = req.body;
-
-  res.json({
-    message: "Data received",
-    username,
-    password,
-  });
+  res.send(`Got a POST request ${req.body}`);
 });
 
+
 app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
